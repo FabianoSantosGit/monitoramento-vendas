18aaa71ef8921cfed675d5d52ba444f2832db2be
Lucas Conceição <lucassouza.conceicao@ucsal.edu.br>
2024-09-17 20:33:31 -0300

    Verificando quais vendedores atingiram a meta

--- Atividade.R
+++ Atividade.R
@@ -1,4 +1,6 @@
 #Criando vetor de vendedores
 vetorDeVendedores <- c(12,41,23,43,52,7,13,24,34,7)
 #Definindo meta minima de vendas
-meta_minima_ <- 20
+meta_minima <- 20
+# Verificando quais vendedores atingiram a meta
+atingiram_meta <- vendas >= meta_minima
