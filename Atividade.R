b6c673f5f6c7583071e4f32201aeade294a205e3
Lucas Conceição <lucassouza.conceicao@ucsal.edu.br>
2024-09-18 18:58:30 -0300

    Calcula quantos vendedores atingiram a meta

--- Atividade.R
+++ Atividade.R
@@ -1,6 +1,9 @@
 #Criando vetor de vendedores
-vetorDeVendedores <- c(12,41,23,43,52,7,13,24,34,7)
+vendas <- c(12,41,23,43,52,7,13,24,34,7)
 #Definindo meta minima de vendas
 meta_minima <- 20
 # Verificando quais vendedores atingiram a meta
 atingiram_meta <- vendas >= meta_minima
+# Calcula quantos vendedores atingiram a meta
+quantos_atingiram_meta <- sum(atingiram_meta)
+cat("Quantidade de vendedores que atingiram a meta:", quantos_atingiram_meta, "\n")
