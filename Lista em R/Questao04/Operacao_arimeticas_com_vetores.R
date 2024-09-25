056ec4834473eb2ef99ca9c070ce2c3eddbe00ec
Lucas Conceição <lucassouza.conceicao@ucsal.edu.br>
2024-09-25 20:26:56 -0300

    Entrega Lucas atividade

---
+++ Lista em R/Questao04/Operacao_arimeticas_com_vetores.R
@@ -0,0 +1,16 @@
+# Criando dois vetores de tamanhos diferentes
+vetor1 <- c(1, 2, 3, 4, 5)    # Vetor com 5 elementos
+vetor2 <- c(10, 20)           # Vetor com 2 elementos
+
+# Realizando uma operação aritmética entre eles (soma)
+resultado <- vetor1 + vetor2
+
+# Exibindo os vetores e o resultado da soma
+print("Vetor 1:")
+print(vetor1)
+
+print("Vetor 2:")
+print(vetor2)
+
+print("Resultado da soma (com reciclagem de vetor):")
+print(resultado)
---
+++ Lista em R/Questao05/Ordenacao_e_selecao_de_elementos.R
@@ -0,0 +1,12 @@
+# Vetor x
+x <- c(50, 10, 5, 1, 2, 3, 11, 25, 30, 35, 40, 4, 42)
+
+# Descobrindo a ordem dos índices
+ordem_indices <- order(x)
+print("Ordem dos índices para classificar o vetor:")
+print(ordem_indices)
+
+# Organizando o vetor em ordem crescente
+x_ordenado <- sort(x)
+print("Vetor em ordem crescente:")
+print(x_ordenado)
